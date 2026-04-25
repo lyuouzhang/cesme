6db6be1006015e52f85c628f775cb7a3afd325f9
lyuouzhang <78629577+lyuouzhang@users.noreply.github.com>
2026-04-25 08:56:29 +0800

    Update utilities.R

--- R/utilities.R
+++ R/utilities.R
@@ -259,15 +259,13 @@ derivative.pen = function(u, lambda, a = 3.7) {
     (u > lambda) * (u <= a * lambda)
 }
 
-initial.sc = function(y,K,nstart=10){
+initial.sc = function(y,K,alpha=0.05,nstart=10){
   
   A = y%*%t(y)
   eig.A = eigen(A)
-  D0 = diag((eig.A$values+eig.A$values[1]*0.05)^(-1/2))
-  eig.A.adj = eigen(D0%*%A%*%D0)
   V = eig.A$vectors[,1:K]
-  D = diag(eig.A$values[1:K]*0.05+eig.A$values[1])
-  km.result = kmeans(V%*%D,centers = K,nstart = 10)
+  D = diag(sqrt(eig.A$values[1:K])/sqrt(eig.A$values[1:K]+eig.A$values[1]*alpha))
+  km.result = kmeans(V%*%D,centers = K,nstart = 20)
   sc.select = km.result$cluster
   sc.mean = t(t(y)%*%V%*%solve(D)%*%t(km.result$centers))
   
